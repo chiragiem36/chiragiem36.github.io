906051e3417710fbc111695cfe4dedec83bca0e8
chirag <chiragiem36@gmail.com>
2018-12-27 20:02:23 +0530

    initiated github pages

---
+++ precache-manifest.7f05713975b1c0187034bbdc7cff3f87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4cc1baff6a458d413bc1",
+    "url": "/static/js/main.4cc1baff.chunk.js"
+  },
+  {
+    "revision": "3533b869310ec67201d9",
+    "url": "/static/js/1.3533b869.chunk.js"
+  },
+  {
+    "revision": "4cc1baff6a458d413bc1",
+    "url": "/static/css/main.f3c00ef4.chunk.css"
+  },
+  {
+    "revision": "78066be259e6c048b36e0b336f1c353f",
+    "url": "/index.html"
+  }
+];
